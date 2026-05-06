0066a885f6200c5f77cd62aa5cf2980ad785b10a
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-05-06 14:50:37 +0100

    Add TDD for AlbumsController class

--- RecordShop.Test/ControllersTest/AlbumControllerTests.cs
+++ RecordShop.Test/ControllersTest/AlbumControllerTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RecordShop.Controllers;
 using RecordShop.Models;
@@ -79,6 +78,65 @@ public class AlbumControllerTests
 
         Assert.That(result, Is.TypeOf<NotFoundResult>());
 
+    }
+
+    [Test]
+    public void GetAlbumById_ReturnsAlbumWithInputtedId_WhenAlbumIdExists()
+    {
+        List<Album> testAlbums = new List<Album>
+        {
+            new Album
+            {
+                Id = 1,
+                Title = "Thriller",
+                Artist = "Michael Jackson",
+                Genre = "Pop",
+                ReleaseYear = 1982,
+                Price = 9.99m,
+                StockQuantity = 10
+            },
+
+            new Album
+            {
+                Id = 2,
+                Title = "Back in Black",
+                Artist = "AC/DC",
+                Genre = "Rock",
+                ReleaseYear = 1980,
+                Price = 8.99m,
+                StockQuantity = 5
+            },
+
+        };
+
+        _albumServiceMock.Setup(service => service.GetAlbumById(2)).Returns(testAlbums[1]);
+
+        IActionResult result = _albumController.GetAlbumById(2);
+
+        _albumServiceMock.Verify(service => service.GetAlbumById(2), Times.Once());
+
+        Assert.That(result, Is.TypeOf<OkObjectResult>());
+    }
+
+    [Test]
+    public void GetAlbumById_ReturnsNotFound_WhenAlbumIdDoesNotExist()
+    {
+        _albumServiceMock.Setup(service => service.GetAlbumById(3)).Returns((Album)null);
+
+        IActionResult result = _albumController.GetAlbumById(3);
+
+        Assert.That(result, Is.TypeOf<NotFoundResult>());
+    }
+
+    [Test]
+    public void GetAlbumById_ReturnsBadRequestWithNegativeId_WhenAlbumIdIsNegative()
+    {
+
+        IActionResult result = _albumController.GetAlbumById(-1);
+
+        Assert.That(result, Is.TypeOf<BadRequestResult>());
+
+        _albumServiceMock.Verify(service => service.GetAlbumById(It.IsAny<int>()), Times.Never());
 
     }
 }
