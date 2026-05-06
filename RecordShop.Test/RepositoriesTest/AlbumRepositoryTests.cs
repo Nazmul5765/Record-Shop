315fa74b7cc488b35b2b1d0c7699fc95e94231f9
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-05-06 11:57:51 +0100

    Add TDD for AlbumRepository Class

--- RecordShop.Test/RepositoriesTest/AlbumRepositoryTests.cs
+++ RecordShop.Test/RepositoriesTest/AlbumRepositoryTests.cs
@@ -2,6 +2,7 @@
 using RecordShop.Data;
 using RecordShop.Models;
 using RecordShop.Repositories;
+using Shouldly;
 
 
 namespace RecordShop.Test;
@@ -62,6 +63,77 @@ public class AlbumRepositoryTests
 
         Assert.That(result, Is.Empty);
     }
+
+    [Test]
+    public void GetAlbumById_ReturnsAlbumWithCorrectId_WhenAlbumIdExist()
+    {
+
+        var options = new DbContextOptionsBuilder<RecordShopDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new RecordShopDbContext(options);
+
+        context.Albums.Add(
+            new Album
+            {
+                Id = 1,
+                Title = "Thriller",
+                Artist = "Michael Jackson",
+                Genre = "Pop",
+                ReleaseYear = 1982,
+                Price = 9.99m,
+                StockQuantity = 10
+            }
+
+        );
+
+        context.SaveChanges();
+
+        var repo = new AlbumRepository(context);
+
+        int id = 1;
+        var result = repo.GetAlbumById(id);
+
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(1);
+
+    }
+
+    [Test]
+    public void GetAlbumById_ReturnsNullWithInvalidId_WhenAlbumIdDoesNotExist()
+    {
+
+        var options = new DbContextOptionsBuilder<RecordShopDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new RecordShopDbContext(options);
+
+        context.Albums.Add(
+            new Album
+            {
+                Id = 1,
+                Title = "Thriller",
+                Artist = "Michael Jackson",
+                Genre = "Pop",
+                ReleaseYear = 1982,
+                Price = 9.99m,
+                StockQuantity = 10
+            }
+
+        );
+
+        context.SaveChanges();
+
+        var repo = new AlbumRepository(context);
+
+        int id = 5;
+        var result = repo.GetAlbumById(id);
+
+        result.ShouldBeNull();
+    }
+
 
 }
 
