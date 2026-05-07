92fef17581495b2f904b68c8f0f80ac7e299304b
Nazmul5765 <nazmulhussain@hotmail.co.uk>
2026-05-07 12:19:46 +0100

    Add TDD for AddAlbum method for repository layer

--- RecordShop.Test/RepositoriesTest/AlbumRepositoryTests.cs
+++ RecordShop.Test/RepositoriesTest/AlbumRepositoryTests.cs
@@ -134,6 +134,178 @@ public class AlbumRepositoryTests
         result.ShouldBeNull();
     }
 
+    [Test]
+    public void AddAlbum_AddsNewAlbum_WhenAlbumIsValid()
+    {
+
+        var options = new DbContextOptionsBuilder<RecordShopDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new RecordShopDbContext(options);
+
+        context.Albums.AddRange(
+            new Album
+            {
+                Id = 1,
+                Title = "Thriller",
+                Artist = "Michael Jackson",
+                Genre = "Pop",
+                ReleaseYear = 1982,
+                Price = 9.99m,
+                StockQuantity = 10
+            },
+
+            new Album
+            {
+                Id = 2,
+                Title = "Back in Black",
+                Artist = "AC/DC",
+                Genre = "Rock",
+                ReleaseYear = 1980,
+                Price = 8.99m,
+                StockQuantity = 5
+            }
+
+        );
+
+        context.SaveChanges();
+
+        var repo = new AlbumRepository(context);
+
+        var newAlbum = new Album
+        {
+            Id = 3,
+            Title = "21",
+            Artist = "Adele",
+            Genre = "Soul",
+            ReleaseYear = 2011,
+            Price = 10.99m,
+            StockQuantity = 7
+        };
+
+        var result = repo.AddAlbum(newAlbum);
+
+
+        result.Id.ShouldBe(3);
+    }
+
+    [Test]
+    public void AddAlbum_ReturnsAddedAlbum_WhenAlbumIsValid()
+    {
+
+        var options = new DbContextOptionsBuilder<RecordShopDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new RecordShopDbContext(options);
+
+        context.Albums.AddRange(
+            new Album
+            {
+                Id = 1,
+                Title = "Thriller",
+                Artist = "Michael Jackson",
+                Genre = "Pop",
+                ReleaseYear = 1982,
+                Price = 9.99m,
+                StockQuantity = 10
+            },
+
+            new Album
+            {
+                Id = 2,
+                Title = "Back in Black",
+                Artist = "AC/DC",
+                Genre = "Rock",
+                ReleaseYear = 1980,
+                Price = 8.99m,
+                StockQuantity = 5
+            }
+
+        );
+
+        context.SaveChanges();
+
+        var repo = new AlbumRepository(context);
+
+        var newAlbum = new Album
+        {
+            Id = 3,
+            Title = "21",
+            Artist = "Adele",
+            Genre = "Soul",
+            ReleaseYear = 2011,
+            Price = 10.99m,
+            StockQuantity = 7
+        };
+
+        var result = repo.AddAlbum(newAlbum);
+
+
+        result.Title.ShouldBe("21");
+        result.Artist.ShouldBe("Adele");
+        result.Genre.ShouldBe("Soul");
+        result.ReleaseYear.ShouldBe(2011);
+
+    }
+
+    [Test]
+    public void AddAlbum_IncreasesAlbumCount_WhenAlbumIsValid()
+    {
+
+        var options = new DbContextOptionsBuilder<RecordShopDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new RecordShopDbContext(options);
+
+        context.Albums.AddRange(
+            new Album
+            {
+                Id = 1,
+                Title = "Thriller",
+                Artist = "Michael Jackson",
+                Genre = "Pop",
+                ReleaseYear = 1982,
+                Price = 9.99m,
+                StockQuantity = 10
+            },
+
+            new Album
+            {
+                Id = 2,
+                Title = "Back in Black",
+                Artist = "AC/DC",
+                Genre = "Rock",
+                ReleaseYear = 1980,
+                Price = 8.99m,
+                StockQuantity = 5
+            }
+
+        );
+
+        context.SaveChanges();
+
+        var repo = new AlbumRepository(context);
+
+        var newAlbum = new Album
+        {
+            Id = 3,
+            Title = "21",
+            Artist = "Adele",
+            Genre = "Soul",
+            ReleaseYear = 2011,
+            Price = 10.99m,
+            StockQuantity = 7
+        };
+
+        var result = repo.AddAlbum(newAlbum);
+
+
+        context.Albums.Count().ShouldBe(3);
+
+    }
 
 }
 
